02e89e37a116a8ec883a4493639aabb98b283aaf
crc8 <checksum@gmx.com>
2013-12-29 00:56:18 +0800

    Fixed single and multiple sync output

--- Parallel Git Repo Sync/Forms/MainForm.cs
+++ Parallel Git Repo Sync/Forms/MainForm.cs
@@ -37,7 +37,70 @@ namespace Parallel_Git_Repo_Sync
             LogTextBox.Clear();
             SyncBackgroundWorker = new BackgroundWorker[1];
             SyncBackgroundWorker[0] = new BackgroundWorker();
-            SyncBackgroundWorker[0].DoWork += new DoWorkEventHandler(SyncBackgroundWorker_DoWork);
+            SyncBackgroundWorker[0].DoWork += (sender2, e2) =>
+            {
+                int j;
+                string GitRepository = GitRepositories[(int)e2.Argument];
+                SyncActionType TargetSyncActionType;
+                string TargetSyncAction;
+                Process SyncProcess;
+
+                for (j = 0; j < 3; j++)
+                {
+                    SyncProcess = new Process();
+                    SyncProcess.EnableRaisingEvents = false;
+                    SyncProcess.StartInfo.FileName = GitBinary;
+                    SyncProcess.StartInfo.CreateNoWindow = true;
+                    SyncProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    SyncProcess.StartInfo.UseShellExecute = false;
+                    SyncProcess.StartInfo.RedirectStandardOutput = true;
+                    SyncProcess.StartInfo.RedirectStandardError = true;
+                    switch (j)
+                    {
+                        case 0:
+                            TargetSyncActionType = SyncActionType.Pull;
+                            TargetSyncAction = "pull --all";
+                            break;
+                        case 1:
+                            TargetSyncActionType = SyncActionType.Push;
+                            TargetSyncAction = "push --all";
+                            break;
+                        case 2:
+                            TargetSyncActionType = SyncActionType.Tags;
+                            TargetSyncAction = "push --tags -f";
+                            break;
+                        default:
+                            return;
+                    }
+                    GitRepositoriesDataGridView.UpdateStatus(GitRepository, TargetSyncActionType, SyncResultType.Pending);
+                    SyncProcess.StartInfo.Arguments = " --git-dir=\"" + GitRepository.Trim() + ".\\.git\" " + " --work-tree=\"" + GitRepository.Trim() + "\" " + TargetSyncAction;
+                    SyncProcess.Start();
+                    while (!SyncProcess.StandardOutput.EndOfStream)
+                    {
+                        LogTextBox.Invoke(new EventHandler(delegate
+                        {
+                            LogTextBox.AppendText(GitRepository + ": (" + TargetSyncAction + ") " + SyncProcess.StandardOutput.ReadLine() + "\r\n");
+                        }));
+                    }
+                    while (!SyncProcess.StandardError.EndOfStream)
+                    {
+                        LogTextBox.Invoke(new EventHandler(delegate
+                        {
+                            LogTextBox.AppendText(GitRepository + ": (" + TargetSyncAction + ") " + SyncProcess.StandardError.ReadLine() + "\r\n");
+                        }));
+                    }
+                    SyncProcess.WaitForExit(15 * 1000);
+                    if (SyncProcess.HasExited && SyncProcess.ExitCode == 0)
+                    {
+                        GitRepositoriesDataGridView.UpdateStatus(GitRepository, TargetSyncActionType, SyncResultType.Success);
+                    }
+                    else
+                    {
+                        GitRepositoriesDataGridView.UpdateStatus(GitRepository, TargetSyncActionType, SyncResultType.Failed);
+                    }
+                    SyncProcess.Close();
+                }
+            };
             SyncBackgroundWorker[0].RunWorkerAsync(e.RowIndex);
         }
 
@@ -48,7 +111,70 @@ namespace Parallel_Git_Repo_Sync
             for (int i = 0; i < GitRepositories.Length; i++)
             {
                 SyncBackgroundWorker[i] = new BackgroundWorker();
-                SyncBackgroundWorker[i].DoWork += new DoWorkEventHandler(SyncBackgroundWorker_DoWork);
+                SyncBackgroundWorker[i].DoWork += (sender2, e2) =>
+                    {
+                        int j;
+                        string GitRepository = GitRepositories[(int)e2.Argument];
+                        SyncActionType TargetSyncActionType;
+                        string TargetSyncAction;
+                        Process SyncProcess;
+
+                        for (j = 0; j < 3; j++)
+                        {
+                            SyncProcess = new Process();
+                            SyncProcess.EnableRaisingEvents = false;
+                            SyncProcess.StartInfo.FileName = GitBinary;
+                            SyncProcess.StartInfo.CreateNoWindow = true;
+                            SyncProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            SyncProcess.StartInfo.UseShellExecute = false;
+                            SyncProcess.StartInfo.RedirectStandardOutput = true;
+                            SyncProcess.StartInfo.RedirectStandardError = true;
+                            switch (j)
+                            {
+                                case 0:
+                                    TargetSyncActionType = SyncActionType.Pull;
+                                    TargetSyncAction = "pull --all";
+                                    break;
+                                case 1:
+                                    TargetSyncActionType = SyncActionType.Push;
+                                    TargetSyncAction = "push --all";
+                                    break;
+                                case 2:
+                                    TargetSyncActionType = SyncActionType.Tags;
+                                    TargetSyncAction = "push --tags -f";
+                                    break;
+                                default:
+                                    return;
+                            }
+                            GitRepositoriesDataGridView.UpdateStatus(GitRepository, TargetSyncActionType, SyncResultType.Pending);
+                            SyncProcess.StartInfo.Arguments = " --git-dir=\"" + GitRepository.Trim() + ".\\.git\" " + " --work-tree=\"" + GitRepository.Trim() + "\" " + TargetSyncAction;
+                            SyncProcess.Start();
+                            while (!SyncProcess.StandardOutput.EndOfStream)
+                            {
+                                LogTextBox.Invoke(new EventHandler(delegate
+                                {
+                                    LogTextBox.AppendText(GitRepository + ": (" + TargetSyncAction + ") " + SyncProcess.StandardOutput.ReadLine() + "\r\n");
+                                }));
+                            }
+                            while (!SyncProcess.StandardError.EndOfStream)
+                            {
+                                LogTextBox.Invoke(new EventHandler(delegate
+                                {
+                                    LogTextBox.AppendText(GitRepository + ": (" + TargetSyncAction + ") " + SyncProcess.StandardError.ReadLine() + "\r\n");
+                                }));
+                            }
+                            SyncProcess.WaitForExit(15 * 1000);
+                            if (SyncProcess.HasExited && SyncProcess.ExitCode == 0)
+                            {
+                                GitRepositoriesDataGridView.UpdateStatus(GitRepository, TargetSyncActionType, SyncResultType.Success);
+                            }
+                            else
+                            {
+                                GitRepositoriesDataGridView.UpdateStatus(GitRepository, TargetSyncActionType, SyncResultType.Failed);
+                            }
+                            SyncProcess.Close();
+                        }
+                    };
                 SyncBackgroundWorker[i].RunWorkerAsync(i);
             }
         }
@@ -88,79 +214,6 @@ namespace Parallel_Git_Repo_Sync
 
             GitRepositoriesDataGridView.ShowRepositories(GitRepositories);
             LogTextBox.Clear();
-        }
-
-        private void SyncBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            int i;
-            string GitRepository = GitRepositories[(int)e.Argument];
-            SyncActionType TargetSyncActionType;
-            string TargetSyncAction;
-            Process SyncProcess;
-
-            for (i = 0; i < 3; i++)
-            {
-                SyncProcess = new Process();
-                SyncProcess.EnableRaisingEvents = false;
-                SyncProcess.StartInfo.FileName = GitBinary;
-                SyncProcess.StartInfo.CreateNoWindow = true;
-                SyncProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                SyncProcess.StartInfo.UseShellExecute = false;
-                SyncProcess.StartInfo.RedirectStandardOutput = true;
-                SyncProcess.StartInfo.RedirectStandardError = true;
-                switch (i)
-                {
-                    case 0:
-                        TargetSyncActionType = SyncActionType.Pull;
-                        TargetSyncAction = "pull --all";
-                        break;
-                    case 1:
-                        TargetSyncActionType = SyncActionType.Push;
-                        TargetSyncAction = "push --all";
-                        break;
-                    case 2:
-                        TargetSyncActionType = SyncActionType.Tags;
-                        TargetSyncAction = "push --tags -f";
-                        break;
-                    default:
-                        return;
-                }
-                GitRepositoriesDataGridView.UpdateStatus(GitRepository, TargetSyncActionType, SyncResultType.Pending);
-                SyncProcess.StartInfo.Arguments = " --git-dir=\"" + GitRepository.Trim() + ".\\.git\" " + " --work-tree=\"" + GitRepository.Trim() + "\" " + TargetSyncAction;
-                SyncProcess.OutputDataReceived += new DataReceivedEventHandler(SyncProcess_OutputDataReceived);
-                SyncProcess.ErrorDataReceived += new DataReceivedEventHandler(SyncProcess_ErrorDataReceived);
-                SyncProcess.Start();
-                SyncProcess.BeginOutputReadLine();
-                SyncProcess.BeginErrorReadLine();
-                SyncProcess.WaitForExit(15 * 1000);
-                if (SyncProcess.HasExited && SyncProcess.ExitCode == 0)
-                {
-                    GitRepositoriesDataGridView.UpdateStatus(GitRepository, TargetSyncActionType, SyncResultType.Success);
-                }
-                else
-                {
-                    GitRepositoriesDataGridView.UpdateStatus(GitRepository, TargetSyncActionType, SyncResultType.Failed);
-                }
-                SyncProcess.Close();
-            }
-        }
-
-        private void SyncProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            LogTextBox.Invoke(new EventHandler(delegate
-            {
-                Process SenderProcess = (Process)sender;
-                if (e.Data != null) LogTextBox.AppendText(e.Data + "\r\n");
-            }));
-        }
-
-        private void SyncProcess_ErrorDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            LogTextBox.Invoke(new EventHandler(delegate
-            {
-                Process SenderProcess = (Process)sender;
-                if(e.Data != null) LogTextBox.AppendText(e.Data + "\r\n");
-            }));
         }
     }
 }
